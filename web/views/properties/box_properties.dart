902a8a2fe3641c6f946922b575a305af014a2beb
John Evans <prujohn@gmail.com>
2012-10-05 10:33:27 -0500

    set templates that wont need code generation(later) to .xml extension

--- web/views/properties/box_properties.dart
+++ web/views/properties/box_properties.dart
@@ -2,5 +2,5 @@
 class BoxProperties extends View
 {
   BoxProperties() :
-    super.fromResource('web/views/templates/box_properties.buckshot');
+    super.fromResource('web/views/templates/box_properties.xml');
 }
--- web/views/properties/transform_properties.dart
+++ web/views/properties/transform_properties.dart
@@ -2,5 +2,5 @@
 class TransformProperties extends View
 {
   TransformProperties() :
-    super.fromResource('web/views/templates/transform_properties.buckshot');
+    super.fromResource('web/views/templates/transform_properties.xml');
 }
